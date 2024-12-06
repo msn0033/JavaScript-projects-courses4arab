302ed4e9046945efb0b62b6289fabc542a93e8cc
Murad Alkhalifi <msn_0033@hotmail.com>
2024-12-07 00:25:09 +0300

    step-2

--- javascript/main.js
+++ javascript/main.js
@@ -6,3 +6,22 @@ const body = document.getElementById("body");
 dark.addEventListener("click", (eo) => {
   body.classList.toggle("dark");
 });}
+
+// const changecolor=document.getElementById("changecolor");
+// const hiddenul=document.getElementById('hiddenul');
+
+
+//When hover on Header =>change-color
+{changecolor.addEventListener("mouseover",async (eo) => {
+  hiddenul.style.display="block";
+  console.log(eo);
+  
+})
+
+changecolor.addEventListener("mouseout",async (eo) => {
+  hiddenul.style.display="none";
+  console.log(ChangeColor.textContent);
+  
+})
+}
+
