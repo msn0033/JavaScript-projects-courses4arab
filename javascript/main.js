aea0def82fd57bf30eedbb3890edd95c635aaea4
Murad Alkhalifi <msn_0033@hotmail.com>
2024-12-07 22:27:16 +0300

    step-3

--- javascript/main.js
+++ javascript/main.js
@@ -13,15 +13,38 @@ dark.addEventListener("click", (eo) => {
 
 //When hover on Header =>change-color
 {changecolor.addEventListener("mouseover",async (eo) => {
-  hiddenul.style.display="block";
-  console.log(eo);
-  
+  hiddenul.style.display="block"; 
 })
-
 changecolor.addEventListener("mouseout",async (eo) => {
   hiddenul.style.display="none";
-  console.log(ChangeColor.textContent);
-  
 })
 }
+
+
+//when click change bg header
+{
+  headerbg.addEventListener('click', (eo) => {
+    let random=Math.round(Math.random()*360);
+    console.log(random);
+    const myheader=document.querySelector('.myheader');
+    myheader.style.backgroundColor=`hsl(${random}, 100%, 50%)`;
+    document.querySelector('#dark').style.backgroundColor=`hsl(${Math.round(Math.random()*100)}, 100%, 50%)`;
+    
+    
+  })
+}
+
+//when click change bg All section
+{const arrcolor=["red","blue","green"];
+let i=0;
+sectionbg.addEventListener('click', (params) => {
+
+const Allsection=document.querySelectorAll('section');
+Allsection.forEach( (item) => {
+  item.style.backgroundColor=arrcolor[i]; 
+});
+i<arrcolor.length-1 ? i++ : i=0;  
+console.log(i);
+
+})}
 
